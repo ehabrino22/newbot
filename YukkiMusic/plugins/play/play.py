b71408bba1ac844189ed0cfbff2b4fde21159ceb
ehabrino22 <98663001+ehabrino22@users.noreply.github.com>
2022-08-04 21:37:28 +0200

    Update play.py

--- YukkiMusic/plugins/play/play.py
+++ YukkiMusic/plugins/play/play.py
@@ -43,7 +43,7 @@ force_btn = InlineKeyboardMarkup(
     [
         [
             InlineKeyboardButton(
-                text="اشترك هنا", url="https://t.me/VIMIlll"
+                text="اشترك هنا", url="https://t.me/creativee4"
             ),                        
         ],        
     ]
@@ -52,10 +52,10 @@ force_btn = InlineKeyboardMarkup(
 async def check_is_joined(message):    
     try:
         userid = message.from_user.id
-        status = await app.get_chat_member("VIMIlll", userid)
+        status = await app.get_chat_member("creativee4", userid)
         return True
     except Exception:
-        await message.reply_text("*انت لست مشترك في قناة البوت @VIMIlll ** \n**انضم لتستطيع تشغيل الاغاني**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
+        await message.reply_text("*انت لست مشترك في قناة البوت @creativee4 ** \n**انضم لتستطيع تشغيل الاغاني**",reply_markup=force_btn,parse_mode="markdown",disable_web_page_preview=False)
         return False
 
 # Command
